e29036f9ba880eec567178b982e655e887f4513b
riturajmahapatra <riturajmahapatra@gmail.com>
2023-09-28 11:46:24 +0530

    newsletter

--- src/NewsLetter/NewsLetter.jsx
+++ src/NewsLetter/NewsLetter.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const NewsletterSection = () => {
+  const [email, setemail] = useState();
+  const obj = { email };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let sendEmail = await fetch("http://localhost:4000/post/email", {
+      method: "post",
+      body: JSON.stringify(obj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    sendEmail = await sendEmail.json();
+    console.log(sendEmail);
+    // console.log();
+  };
+
   return (
     <div className="py-16 ">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
@@ -13,31 +30,36 @@ const NewsletterSection = () => {
               Thank you for choosing G3S Securities for your security needs.
               Subscribe now and be part of our security community!
             </p>
-            <div className="mt-6 flex max-w-md gap-x-4">
-              <label htmlFor="email-address" className="sr-only">
-                Email address
-              </label>
-              <input
-                id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="min-w-0 flex-auto rounded-md border-[1px] px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                placeholder="Enter your email"
-              />
-              <button
-                type="submit"
-                className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
-                Subscribe
-              </button>
-            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="mt-6 flex max-w-md gap-x-4">
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="min-w-0 flex-auto rounded-md border-[1px] px-3.5 py-2 shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  placeholder="Enter your email"
+                  onChange={(e) => {
+                    setemail(e.target.value);
+                  }}
+                />
+                <button
+                  type="submit"
+                  className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                >
+                  Subscribe
+                </button>
+              </div>
+            </form>
           </div>
           <div>
             <img
               className="w-full  h-[60vh] object-bottom rounded-lg object-cover"
-              src="https://images.unsplash.com/photo-1589476993526-4b4317f5e188?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1854&q=80"
+              src="newsletter.webp"
               alt="Newsletter Placeholder"
             />
           </div>
